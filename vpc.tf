5113beac5044bb342a75855e2913e5de790aedac
Ayomidev <aayo20dev@gmail.com>
2023-12-08 23:07:50 +0100

    new commits

--- vpc.tf
+++ vpc.tf
@@ -21,7 +21,7 @@ resource "aws_internet_gateway" "my_gw" {
 resource "aws_subnet" "my_subnet" {
   count                   = var.subnet_count
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 2, count.index)
+  cidr_block              = element(["172.16.32.0/20", "172.16.64.0/20", "172.16.96.0/20", "172.16.128.0/20"], count.index % 4)
   availability_zone       = element(data.aws_availability_zones.my_azs.names, count.index % length(data.aws_availability_zones.my_azs.names))
   map_public_ip_on_launch = true
 }
