7ce70dc3d4f011512aff57d70b2b5f727f064566
Ayomidev <aayo20dev@gmail.com>
2023-12-08 22:18:28 +0100

    more vpc changes

--- vpc.tf
+++ vpc.tf
@@ -12,8 +12,8 @@ resource "aws_internet_gateway" "my_gw" {
 
 # Subnets
 resource "aws_subnet" "my_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = cidrsubnet(var.vpc_cidr)
+  vpc_id = aws_vpc.my_vpc.id
+  #cidr_block              = cidrsubnet(var.vpc_cidr)
   availability_zone       = "us-west-1a" # Replace with your desired availability zone
   map_public_ip_on_launch = true
 }
